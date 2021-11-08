b79fb59e9fb43ef0e9b6d0a806f52ae1319418f6
zhangyouxin <zhangyouxingg@126.com>
2021-11-08 17:28:04 +0800

    refactor: add create selling_lock_arg function to make tests more understandable

--- contract/tests/src/tests.rs
+++ contract/tests/src/tests.rs
@@ -9,6 +9,23 @@ use ckb_testtool::ckb_types::{
 };
 const MAX_CYCLES: u64 = 10_000_000;
 
+fn create_selling_lock_args(owner_lock: &Script,  price: u64) -> Bytes {
+    let owner_lock_code_hash: Byte32 = owner_lock.code_hash();
+    let owner_lock_hash_type: Byte = owner_lock.hash_type();
+    let owner_lock_args: Bytes = owner_lock.args().unpack();
+    let price_vec = price.to_le_bytes().to_vec();
+
+    Bytes::from(
+        owner_lock_code_hash.as_slice()
+            .iter()
+            .chain(owner_lock_hash_type.as_slice())
+            .chain(owner_lock_args.as_ref())
+            .chain(price_vec.as_slice())
+            .cloned()
+            .collect::<Vec<u8>>(),
+    )
+}
+
 #[test]
 fn test_unlock_by_purchase() {
     // deploy contract
@@ -19,29 +36,26 @@ fn test_unlock_by_purchase() {
     // deploy always_success script
     let always_success_out_point: OutPoint = context.deploy_cell(ALWAYS_SUCCESS.clone());
 
-    static ARGS_BYTES: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 49, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 244, 1, 0, 0, 0, 0, 0, 0 ];
-    static OWNER_LOCK_CODE_HASH: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49];
-    static OWNER_LOCK_ARGS: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
-    static OWNER_LOCK_HASH_TYPE: &'static [u8] = &[49];
      // prepare scripts
+    let a_s_lock_script = context
+        .build_script(&always_success_out_point, Default::default())
+        .expect("script");
+    let a_s_lock_script_dep = CellDep::new_builder()
+        .out_point(always_success_out_point.clone())
+        .build();
+
+    let owner_lock = context
+        .build_script(&always_success_out_point, Bytes::from(vec![8]))
+        .expect("script");
+    let selling_lock_args = create_selling_lock_args(&owner_lock, 500u64);
+
     let lock_script = context
-        .build_script(&out_point, Bytes::from(ARGS_BYTES))
+        .build_script(&out_point, Bytes::from(selling_lock_args))
         .expect("script");
     let lock_script_dep = CellDep::new_builder()
         .out_point(out_point)
         .build();
 
-    let a_s_lock_script = context
-        .build_script(&always_success_out_point, Default::default())
-        .expect("script");
-    let a_s_lock_script_dep = CellDep::new_builder()
-        .out_point(always_success_out_point)
-        .build();
-
-    let owner_lock = ScriptBuilder::default()
-                            .code_hash(Byte32::from_slice(OWNER_LOCK_CODE_HASH).unwrap())
-                            .args(Bytes::from(OWNER_LOCK_ARGS).pack())
-                            .hash_type(Byte::from_slice(OWNER_LOCK_HASH_TYPE).unwrap());
     // prepare cells
     let input_out_point = context.create_cell(
         CellOutput::new_builder()
@@ -71,7 +85,7 @@ fn test_unlock_by_purchase() {
             .build(),
         CellOutput::new_builder()
             .capacity(1500u64.pack())
-            .lock(owner_lock.build())
+            .lock(owner_lock)
             .build(),
     ];
 
@@ -104,34 +118,25 @@ fn test_unlock_by_owner() {
     // deploy always_success script
     let always_success_out_point: OutPoint = context.deploy_cell(ALWAYS_SUCCESS.clone());
 
-    static ARGS_BYTES: &'static [u8] = &[
-        230, 131, 176, 65, 57, 52, 71, 104, 52, 132, 153, 194, 62, 177, 50, 109, 90, 82, 214, 219, 0, 108, 13, 47, 236, 224, 10, 131, 31, 54, 96, 215,
-        2,
-        8,
-        244, 1, 0, 0, 0, 0, 0, 0 
-        ];
-    static OWNER_LOCK_CODE_HASH: &'static [u8] = &[230, 131, 176, 65, 57, 52, 71, 104, 52, 132, 153, 194, 62, 177, 50, 109, 90, 82, 214, 219, 0, 108, 13, 47, 236, 224, 10, 131, 31, 54, 96, 215];
-    static OWNER_LOCK_ARGS: &'static [u8] = &[8];
-    static OWNER_LOCK_HASH_TYPE: &'static [u8] = &[2];
      // prepare scripts
+    let a_s_lock_script = context
+        .build_script(&always_success_out_point, Default::default())
+        .expect("script");
+    let a_s_lock_script_dep = CellDep::new_builder()
+        .out_point(always_success_out_point.clone())
+        .build();
+
+    let owner_lock = context
+        .build_script(&always_success_out_point, Bytes::from(vec![8]))
+        .expect("script");
+    let selling_lock_args = create_selling_lock_args(&owner_lock, 500u64);
+
     let lock_script = context
-        .build_script(&out_point, Bytes::from(ARGS_BYTES))
+        .build_script(&out_point, Bytes::from(selling_lock_args))
         .expect("script");
     let lock_script_dep = CellDep::new_builder()
         .out_point(out_point)
         .build();
-
-    let a_s_lock_script = context
-        .build_script(&always_success_out_point, Bytes::from(vec![8]))
-        .expect("script");
-    let a_s_lock_script_dep = CellDep::new_builder()
-        .out_point(always_success_out_point)
-        .build();
-
-    let owner_lock = ScriptBuilder::default()
-                            .code_hash(Byte32::from_slice(OWNER_LOCK_CODE_HASH).unwrap())
-                            .args(Bytes::from(OWNER_LOCK_ARGS).pack())
-                            .hash_type(Byte::from_slice(OWNER_LOCK_HASH_TYPE).unwrap());
     // prepare cells
     let input_out_point = context.create_cell(
         CellOutput::new_builder()
@@ -161,97 +166,7 @@ fn test_unlock_by_owner() {
             .build(),
         CellOutput::new_builder()
             .capacity(1500u64.pack())
-            .lock(owner_lock.build())
-            .build(),
-    ];
-
-    let outputs_data = vec![Bytes::new(); 2];
-
-    // build transaction
-    let tx = TransactionBuilder::default()
-        .inputs(inputs)
-        .outputs(outputs)
-        .outputs_data(outputs_data.pack())
-        .cell_dep(lock_script_dep)
-        .cell_dep(a_s_lock_script_dep)
-        .build();
-    let tx = context.complete_tx(tx);
-
-    // run
-    let cycles = context
-        .verify_tx(&tx, MAX_CYCLES)
-        .expect("pass verification");
-    println!("consume cycles: {}", cycles);
-}
-
-#[test]
-fn test_sell_by_owner() {
-    // deploy contract
-    let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("selling-lock");
-    let out_point = context.deploy_cell(contract_bin);
-
-    // deploy always_success script
-    let always_success_out_point: OutPoint = context.deploy_cell(ALWAYS_SUCCESS.clone());
-
-    static ARGS_BYTES: &'static [u8] = &[
-        230, 131, 176, 65, 57, 52, 71, 104, 52, 132, 153, 194, 62, 177, 50, 109, 90, 82, 214, 219, 0, 108, 13, 47, 236, 224, 10, 131, 31, 54, 96, 215,
-        2,
-        8,
-        244, 1, 0, 0, 0, 0, 0, 0 
-        ];
-    static OWNER_LOCK_CODE_HASH: &'static [u8] = &[230, 131, 176, 65, 57, 52, 71, 104, 52, 132, 153, 194, 62, 177, 50, 109, 90, 82, 214, 219, 0, 108, 13, 47, 236, 224, 10, 131, 31, 54, 96, 215];
-    static OWNER_LOCK_ARGS: &'static [u8] = &[8];
-    static OWNER_LOCK_HASH_TYPE: &'static [u8] = &[2];
-     // prepare scripts
-    let lock_script = context
-        .build_script(&out_point, Bytes::from(ARGS_BYTES))
-        .expect("script");
-    let lock_script_dep = CellDep::new_builder()
-        .out_point(out_point)
-        .build();
-
-    let a_s_lock_script = context
-        .build_script(&always_success_out_point, Bytes::from(vec![8]))
-        .expect("script");
-    let a_s_lock_script_dep = CellDep::new_builder()
-        .out_point(always_success_out_point)
-        .build();
-
-    let owner_lock = ScriptBuilder::default()
-                            .code_hash(Byte32::from_slice(OWNER_LOCK_CODE_HASH).unwrap())
-                            .args(Bytes::from(OWNER_LOCK_ARGS).pack())
-                            .hash_type(Byte::from_slice(OWNER_LOCK_HASH_TYPE).unwrap());
-    // prepare cells
-    let input_out_point = context.create_cell(
-        CellOutput::new_builder()
-            .capacity(1000u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        Bytes::new(),
-    );
-    let input_pay_cell_out_point = context.create_cell(
-        CellOutput::new_builder()
-            .capacity(2000u64.pack())
-            .lock(a_s_lock_script.clone())
-            .build(),
-        Bytes::new(),
-    );
-    let input = CellInput::new_builder()
-        .previous_output(input_out_point)
-        .build();
-    let pay_cell_input = CellInput::new_builder()
-        .previous_output(input_pay_cell_out_point)
-        .build();
-    let inputs = vec![input, pay_cell_input];
-    let outputs = vec![
-        CellOutput::new_builder()
-            .capacity(400u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        CellOutput::new_builder()
-            .capacity(1500u64.pack())
-            .lock(owner_lock.build())
+            .lock(owner_lock)
             .build(),
     ];
 
@@ -292,33 +207,30 @@ fn verify_purchase_multiple_should_fail() {
     // deploy always_success script
     let always_success_out_point: OutPoint = context.deploy_cell(ALWAYS_SUCCESS.clone());
 
-    static ARGS_BYTES_PRICE_500: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 49, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 244, 1, 0, 0, 0, 0, 0, 0 ];
-    static ARGS_BYTES_PRICE_600: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 49, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 88, 2, 0, 0, 0, 0, 0, 0 ];
-    static OWNER_LOCK_CODE_HASH: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49];
-    static OWNER_LOCK_ARGS: &'static [u8] = &[48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
-    static OWNER_LOCK_HASH_TYPE: &'static [u8] = &[49];
      // prepare scripts
-    let lock_script = context
-        .build_script(&out_point, Bytes::from(ARGS_BYTES_PRICE_500))
-        .expect("script");
-    let lock_script2 = context
-        .build_script(&out_point, Bytes::from(ARGS_BYTES_PRICE_600))
-        .expect("script");
-    let lock_script_dep = CellDep::new_builder()
+   let lock_script_dep = CellDep::new_builder()
         .out_point(out_point.clone())
         .build();
-
     let a_s_lock_script = context
         .build_script(&always_success_out_point, Default::default())
         .expect("script");
     let a_s_lock_script_dep = CellDep::new_builder()
-        .out_point(always_success_out_point)
+        .out_point(always_success_out_point.clone())
         .build();
 
-    let owner_lock = ScriptBuilder::default()
-                            .code_hash(Byte32::from_slice(OWNER_LOCK_CODE_HASH).unwrap())
-                            .args(Bytes::from(OWNER_LOCK_ARGS).pack())
-                            .hash_type(Byte::from_slice(OWNER_LOCK_HASH_TYPE).unwrap());
+    let owner_lock =  context
+        .build_script(&always_success_out_point, Bytes::from(vec![8]))
+        .expect("script");
+    let selling_lock_args_with_price_500 = create_selling_lock_args(&owner_lock, 500u64);
+    let selling_lock_args_with_price_600 = create_selling_lock_args(&owner_lock, 600u64);
+
+    let lock_script = context
+        .build_script(&out_point, Bytes::from(selling_lock_args_with_price_500))
+        .expect("script");
+    let lock_script2 = context
+        .build_script(&out_point, Bytes::from(selling_lock_args_with_price_600))
+        .expect("script");
+
     // prepare cells
     let sell_lock_out_point1 = context.create_cell(
         CellOutput::new_builder()
@@ -358,7 +270,7 @@ fn verify_purchase_multiple_should_fail() {
             .build(),
         CellOutput::new_builder()
             .capacity(3000u64.pack())
-            .lock(owner_lock.build())
+            .lock(owner_lock)
             .build(),
     ];
 
